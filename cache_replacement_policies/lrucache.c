1ed8083e55670a029fde7c0efe8557641cf8507e
Diego Mondragon <diego@foodandmore.app>
2020-02-15 21:27:25 -0800

    added cache_read and cache_write functions

--- cache_replacement_policies/lrucache.c
+++ cache_replacement_policies/lrucache.c
@@ -1,5 +1,3 @@
-#define CACHE_BLOCK_DATA_SIZE 2
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -15,11 +13,11 @@ typedef struct {
 	int block_count;
 	int hits;
 	int misses;
+	int last_age_bit;
 } LRUCache;
 
-_Bool cache_write(CacheBlock block);
-
-CacheBlock cache_read(char data_to_read);
+CacheBlock cache_write(LRUCache * cache, char data_to_write);
+CacheBlock cache_read(LRUCache * cache, char data_to_read);
 
 void print_cache_stats(LRUCache cache)
 {
@@ -31,17 +29,49 @@ void print_cache_stats(LRUCache cache)
 	printf("\n");
 }
 
-int main(int argc, char *argv[])
+CacheBlock cache_read(LRUCache * cache, char data_to_read)
 {
-	LRUCache *cache = calloc(1, sizeof(LRUCache));
+	int block_index;
+
+	while (block_index < cache->block_count) {
+		if(cache->blocks[block_index].data == data_to_read){
+			cache->hits += 1;
+			return cache->blocks[block_index];
+		}
+
+		block_index += 1;
+	}
+
+	cache->misses = cache->misses + 1;
+
+	return cache_write(cache, data_to_read);
+}
+
+CacheBlock cache_write(LRUCache * cache, char data_to_write)
+{
+	CacheBlock * new_cache_block = calloc(1, sizeof(CacheBlock));
+	new_cache_block->age_bit = cache->last_age_bit + 1;
+	new_cache_block->data = data_to_write;
+
+	cache->blocks = (CacheBlock*) realloc(cache->blocks, (sizeof(cache->blocks) + sizeof(CacheBlock)));
+
+	cache->block_count += 1;
+	cache->blocks[cache->block_count] = *new_cache_block;
+	cache->last_age_bit = new_cache_block->age_bit;
+
+	return *new_cache_block;
+}
+
+int main(int argc, char **argv)
+{
+	LRUCache * cache = calloc(1, sizeof(LRUCache));
+
 	print_cache_stats(*cache);
 
 	char* cache_max_elements_input = malloc(3*sizeof(char));
 
 	printf("Enter max elements of the cache: ");
 	scanf("%2c", cache_max_elements_input);
-
-	printf("Entered max cache elements %s", cache_max_elements_input);
 
 	free(cache);
 	free(cache_max_elements_input);
